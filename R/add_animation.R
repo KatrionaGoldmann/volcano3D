0b82398086f6eae0d75de229beef6ec5e8e880ab
Myles Lewis <myles.lewis@qmul.ac.uk>
2022-07-05 15:03:11 +0100

    Revert "Update http link"
    This reverts commit 7dece0be74f19a9990b78df89f0b5faccdd817aa.

--- R/add_animation.R
+++ R/add_animation.R
@@ -19,7 +19,7 @@
 #' z-axis
 #' @references
 #' Lewis, Myles J., et al. (2019).
-#' \href{https://doi.org/10.1016/j.celrep.2019.07.091}{
+#' \href{https://www.cell.com/cell-reports/fulltext/S2211-1247(19)31007-1}{
 #' Molecular portraits of early rheumatoid arthritis identify clinical and 
 #' treatment response phenotypes.}
 #' \emph{Cell reports}, \strong{28}:9
