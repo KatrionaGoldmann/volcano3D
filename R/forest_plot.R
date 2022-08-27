c48fdcc15af98d0dde34bc7e00e10ef225615606
Myles Lewis <myles.lewis@qmul.ac.uk>
2022-08-27 13:55:16 +0100

    Update forest_plot

--- R/forest_plot.R
+++ R/forest_plot.R
@@ -4,30 +4,38 @@
 #' @param object A 'volc3d' class object
 #' @param gene Gene to plot
 #' @param scheme Vector of 3 colours for plotting
+#' @param labs Optional character vector of labels for the groups
+#' @param error_type Either "ci" or "se" to specify whether error bars use 95%
+#'   confidence intervals or standard error.
 #' @param ... Optional arguments passed to [plot()]
-#' @importFrom graphics abline arrows axis par text
+#' @importFrom graphics abline arrows axis par points text
 #' @export
 
 forest_plot <- function(object, gene,
                         scheme = c('red', 'green3', 'blue'),
+                        labs = NULL,
+                        error_type = c("ci", "se"),
                         ...) {
+  error_type <- match.arg(error_type)
   df <- object@df[[2]]
   x <- unlist(df[gene, 1:3])
-  xCI <- unlist(df[gene, 4:6]) * 1.96
+  xCI <- unlist(df[gene, 4:6])
+  if (error_type == "ci") xCI <- xCI * 1.96
   xrange <- range(c(x-xCI, x+xCI, 0), na.rm=TRUE)
   op <- par(mar=c(5, 7, 2, 4))
   on.exit(par(op))
-  plot(x, y = 3:1,
-       pch=19, col=scheme, 
-       xlim = xrange, yaxt="n", xlab=paste(gene, "log2 FC"), ylab = "", las=1, bty="n",
-       panel.first = {
-         arrows(x-xCI, 3:1, x+xCI, code=3, angle=90, length= 0.05,
-                col=scheme)
-       }, ...)
-  abline(v=0, lty=2)
-  axis(2, 3:1, colnames(df)[1:3], tick=F, las=1)
+  new.args <- list(...)
+  plot.args <- list(x = NA, xlim = xrange, ylim = c(1,3), yaxt="n", bty = "n",
+                    xlab = paste(gene, "log2 FC"), ylab = "")
+  if (length(new.args)) plot.args[names(new.args)] <- new.args
+  do.call("plot", plot.args)
+  arrows(x-xCI, 3:1, x+xCI, code=3, angle=90, length=0.05,
+         col=scheme)
+  points(x, y = 3:1, pch=19, col=scheme)
+  abline(v = 0, lty = 2)
+  if (is.null(labs)) labs <- colnames(df)[1:3]
+  axis(2, 3:1, labs, tick = FALSE, las = 1)
   stars <- stars.pval(object@padj[gene,])
-  stars[stars== "."] <- ""
   text(xrange[2]+ diff(xrange)*0.06, 3:1, stars, xpd = NA)
 }
 
